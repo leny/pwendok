ca828211230fc12a1951410a4607ac5fc53a1437
Leny <info@flatland.be>
2014-05-24 02:57:25 +0200

    Rename and add some command to mac.setup

--- commands/mac.setup.sh
+++ commands/mac.setup.sh
@@ -1,10 +1,19 @@
-# MISC CLI COMMANDS TO EXECUTE ON A NEWLY INSTALLED MAC
+#!/usr/bin/env bash
+# MISC SETUP COMMANDS TO EXECUTE ON A NEWLY INSTALLED MAC
 
-# remove spotlight icon in the menubar
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until this script has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+# Disable the sound effects on boot
+sudo nvram SystemAudioVolume=" "
+
+# Remove spotlight icon in the menubar
 sudo mv /System/Library/CoreServices/Search.bundle /System/Library/CoreServices/Search.bundle.bak
-killall SystemUIServer
 
-# disable CMD+M system-wide keyboard shortcut
+# Disable CMD+M system-wide keyboard shortcut
 defaults write -g NSUserKeyEquivalents -dict-add 'Minimize' '\0'
 
 # Expand save panel by default
@@ -30,6 +39,26 @@ defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
 # Disable press-and-hold for keys in favor of key repeat
 defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 
+# Increase sound quality for Bluetooth headphones/headsets
+defaults write com.apple.BluetoothAudioAgent "Apple Bitpool Min (editable)" -int 40
+
+# Enable full keyboard access for all controls (e.g. enable Tab in modal dialogs)
+defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
+
+# Use scroll gesture with the Ctrl (^) modifier key to zoom
+defaults write com.apple.universalaccess closeViewScrollWheelToggle -bool true
+defaults write com.apple.universalaccess HIDScrollZoomModifierMask -int 262144
+# Follow the keyboard focus while zoomed in
+defaults write com.apple.universalaccess closeViewZoomFollowsFocus -bool true
+
+# Automatically illuminate built-in MacBook keyboard in low light
+defaults write com.apple.BezelServices kDim -bool true
+# Turn off keyboard illumination when computer is not used for 2 minutes
+defaults write com.apple.BezelServices kDimTime -int 120
+
+# Disable auto-correct
+defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+
 # Save screenshots to the desktop
 defaults write com.apple.screencapture location -string "$HOME/Desktop"
 
@@ -41,6 +70,9 @@ defaults write com.apple.finder ShowStatusBar -bool true
 
 # Finder: allow text selection in Quick Look
 defaults write com.apple.finder QLEnableTextSelection -bool true
+
+# Finder: show all filename extensions
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 
 # When performing a search, search the current folder by default
 defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
@@ -64,4 +96,25 @@ defaults write com.apple.terminal StringEncodings -array 4
 # Prevent Time Machine from prompting to use new hard drives as backup volume
 defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
 
+# Empty Trash securely by default
+defaults write com.apple.finder EmptyTrashSecurely -bool true
 
+# Don’t show Dashboard as a Space
+defaults write com.apple.dock dashboard-in-overlay -bool true
+
+# Disable Dashboard
+defaults write com.apple.dashboard mcx-disabled -bool true
+
+# Automatically hide and show the Dock
+defaults write com.apple.dock autohide -bool true
+
+
+# - - -
+
+# Kill affected applications
+
+for app in "Address Book" "Calendar" "Contacts" "Dashboard" "Dock" "Finder" \
+    "Mail" "Safari" "SizeUp" "SystemUIServer" "Terminal" "Transmission" \
+    "Twitter" "iCal" "iTunes"; do
+    killall "$app" > /dev/null 2>&1
+done
