587e03db6b8ad4065f4c44347bb7f771b113b95a
Leny <info@flatland.be>
2014-07-16 22:10:34 +0200

    Fix install.sh

--- install.sh
+++ install.sh
@@ -10,16 +10,16 @@ mkdir -pv "$DOTFILES_DIR/bin"
 curl -L https://github.com/djl/vcprompt/raw/master/bin/vcprompt > "$DOTFILES_DIR/bin/vcprompt"
 chmod +x "$DOTFILES_DIR/bin/vcprompt"
 
-echo "----- Link dotfiles -----"
+echo "\n\n\n----- Link dotfiles -----"
 
-ln -sfhv "$DOTFILES_DIR/runcom/.bash_profile"
-ln -sfhv "$DOTFILES_DIR/runcom/.inputrc"
-ln -sfhv "$DOTFILES_DIR/editor/.editorconfig"
-ln -sfhv "$DOTFILES_DIR/git/.gitconfig"
-ln -sfhv "$DOTFILES_DIR/git/.gitignore_global"
-ln -sfhv "$DOTFILES_DIR/hg/.hgignore_global"
+ln -sfhv "$DOTFILES_DIR/runcom/.bash_profile" ~
+ln -sfhv "$DOTFILES_DIR/runcom/.inputrc" ~
+ln -sfhv "$DOTFILES_DIR/editor/.editorconfig" ~
+ln -sfhv "$DOTFILES_DIR/git/.gitconfig" ~
+ln -sfhv "$DOTFILES_DIR/git/.gitignore_global" ~
+ln -sfhv "$DOTFILES_DIR/hg/.hgignore_global" ~
 
-echo "----- Install homebrew & cask -----"
+echo "\n\n\n----- Install homebrew & cask -----"
 
 ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
 brew doctor
@@ -27,22 +27,23 @@ brew tap caskroom/cask
 brew install brew-cask
 brew tap caskroom/versions
 
-echo "----- Install brew's -----"
+echo "\n\n\n----- Install brew's -----"
 
 brew bundle "$DOTFILES_DIR/Brewfile"
 
-echo "----- Install app's -----"
+echo "\n\n\n----- Install app's -----"
 
+export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 brew bundle "$DOTFILES_DIR/Caskfile"
 
-echo "----- Install homebrew's bash -----"
+echo "\n\n\n----- Install homebrew's bash -----"
 
 grep "/usr/local/bin/bash" /private/etc/shells &>/dev/null || sudo bash -c "echo /usr/local/bin/bash >> /private/etc/shells"
 chsh -s /usr/local/bin/bash
 
-echo "----- Install useful global npm packages -----"
+echo "\n\n\n----- Install useful global npm packages -----"
 npm install -g bower browserify codo coffee-script coffeegulp docco grunt grunt-cli grunt-devtools grunt-init gulp hexo mocha phantomas stylus tankipas vibox vows woazar yo
 
-echo "----- CLI Sublime Text Alias -----"
+echo "\n\n\n----- CLI Sublime Text Alias -----"
 [ -f ~/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl ] && ln -sfhv ~/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl "$DOTFILES_DIR/bin/sublime"
 [ -f ~/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl ] && ln -sfhv ~/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl "$DOTFILES_DIR/bin/subl"
