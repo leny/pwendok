66db060287551508439c746230a3351f5d38bb38
leny <info@flatland.be>
2016-03-23 11:43:57 +0100

    Force removal in dcrm alias

--- zsh/custom/aliases.zsh
+++ zsh/custom/aliases.zsh
@@ -65,7 +65,7 @@ alias dcb="docker-compose build"
 alias dcu="docker-compose up -d"
 alias dcl="docker-compose logs"
 alias dcs="docker-compose stop"
-alias dcrm="docker-compose rm"
+alias dcrm="docker-compose rm -f"
 
 # --- git (overrides from oh-my-zsh/git - )
 alias gcm="git commit -m"
