c240640618eed424390914175bd35bce62a8f27c
leny <info@flatland.be>
2016-04-27 10:40:05 +0200

    Clean unused env vars & functions

--- zsh/custom/functions.zsh
+++ zsh/custom/functions.zsh
@@ -6,9 +6,6 @@ mkd() { mkdir -p "$@" && cd "$@"; }
 # Jump to a directory and list its content
 cdl() { cd "$@" && l; }
 
-# Checks whether a website is down for you, or everybody
-down4me() { curl -s "http://www.downforeveryoneorjustme.com/$1" | sed '/just you/!d;s/<[^>]*>//g'; }
-
 # gclone : clone a github repo by passing the user/repo tuple
 gclone() { git clone https://github.com/$1 $2; }
 
