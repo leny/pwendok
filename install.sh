65a0d44d75d48d2acbec5a46955c8fde11160b83
leny <info@flatland.be>
2016-11-03 23:04:03 +0100

    Add note in install script

--- install.sh
+++ install.sh
@@ -40,6 +40,7 @@ echo "----- configure vim -----"
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
+# TODO: automatize the YouCompleteMe installation
 
 echo ""
 echo "----- link atom -----"
