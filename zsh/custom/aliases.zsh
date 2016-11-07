a0c3235a20faf91fb6883019e724da1fc9414af9
leny <info@flatland.be>
2016-11-07 23:25:18 +0100

    Add alias to open current folder in vim

--- zsh/custom/aliases.zsh
+++ zsh/custom/aliases.zsh
@@ -114,3 +114,6 @@ alias goodnight="pmset displaysleepnow && pmset sleepnow"
 
 # --- Gource
 alias pyebwa="gource --hide dirnames,progress,filenames -r 60 --seconds-per-day 0.25 --auto-skip-seconds 1 -1600x1000 -multi-sampling --title"
+
+# --- vim
+alias o="mvim ."
