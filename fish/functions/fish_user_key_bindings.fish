d6a8f62ece3f59f2db33dd9f41d70585e0e96859
leny <info@flatland.be>
2018-02-15 21:50:17 +0100

    Misc fish tweaks

---
+++ fish/functions/fish_user_key_bindings.fish
@@ -0,0 +1,5 @@
+function fish_user_key_bindings
+    # cf. http://lkdjiin.github.io/blog/2017/02/28/fish-completion-puis-execution-en-
+	# Ctrl+g (Go!). Like Ctrl+f Enter in one go.
+    bind \cg accept-autosuggestion execute
+end
