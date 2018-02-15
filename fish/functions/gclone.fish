3f714d18bf51406f9d98bc02afab3c976d833ff7
leny <info@flatland.be>
2018-02-15 22:02:57 +0100

    Misc fish utils

---
+++ fish/functions/gclone.fish
@@ -0,0 +1,3 @@
+function gclone --description 'clone a github repo by passing the user/repo tuple'
+	git clone https://github.com/$argv
+end
---
+++ fish/functions/mkd.fish
@@ -0,0 +1,4 @@
+function mkd --description 'create directory and enter it'
+    mkdir -p $argv
+    cd $argv
+end
