ddd0ace0eaf915860dc9b5d30e06b92d39f87c3d
leny <info@flatland.be>
2018-02-15 22:12:26 +0100

    Fix env paths

--- fish/config.fish
+++ fish/config.fish
@@ -2,8 +2,7 @@ set -x EDITOR "vim"
 set -x CLICOLOR 1
 set -x LSCOLORS gxfxcxdxbxegedabagacad
 
-set -x WORKS_PATH "~/Works"
+set -x WORKS_PATH "$HOME/Works"
 set -x GREP_OPTIONS "--color=auto"
 set -x HOMEBREW_CASK_OPTS "--appdir=/Applications"
-set -x GOPATH $home/.golang
-set -x PATH (yarn global bin) (brew --prefix coreutils)/libexec/gnubin /usr/local/sbin $GOPATH $PATH
+set -x PATH (yarn global bin) (brew --prefix coreutils)/libexec/gnubin /usr/local/sbin $PATH
