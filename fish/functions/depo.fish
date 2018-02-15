8de0a3e719042e7b889470b34a58fe558b9617f8
leny <info@flatland.be>
2018-02-15 22:23:11 +0100

    Basic fish implementation of depo

---
+++ fish/functions/depo.fish
@@ -0,0 +1,6 @@
+function depo -d "try to change directory to github repo, with user/repo, from the WORKS_PATH"
+    cd $WORKS_PATH/$argv[1]
+end
+
+# TODO: completion
+# cf. https://github.com/fish-shell/fish-shell/tree/master/share/completions
