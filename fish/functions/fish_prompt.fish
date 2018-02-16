1dcc7e732c2de75e30cb05ce1a343eadf0d95da6
leny <info@flatland.be>
2018-02-16 22:57:31 +0100

    Add last command duration in prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -5,20 +5,44 @@ function fish_prompt
     set -l resetColor (set_color normal)
     set -l nWhite (set_color white)
     set -l nBlue (set_color blue)
-    set -l bRed (set_color -o red)
+    set -l bRed (set_color --bold red)
     set -l nCyan (set_color cyan)
     set -l bCyan (set_color --bold cyan)
     set -l nYellow (set_color yellow)
+    set -l bYellow (set_color --bold yellow)
     set -l nMagenta (set_color magenta)
+    set -l nGreen (set_color green)
+
+    # last command duration
+    if test $CMD_DURATION
+        set -l timer (math "$CMD_DURATION / 1000")
+        if test $timer -ge 3600
+            set -l timerHours (math "$timer / 3600")
+            set -l timerRemain (math "$timer % 3600")
+            set -l timerMinutes (math "$timer / 60")
+            set -l timerSeconds (math "$timer % 60")
+            echo -s "$resetColor" "$nMagenta" "◴ duration: " "$bRed" "$timerHours" "h " "$timerMinutes" "m " "$timerSeconds" "s " "$resetColor"
+        else if test $timer -ge 60
+            set -l timerMinutes (math "$timer / 60")
+            set -l timerSeconds (math "$timer % 60")
+            echo -s "$resetColor" "$nMagenta" "◴ duration: " "$bYellow" "$timerMinutes" "m " "$timerSeconds" "s " "$resetColor"
+        else if test $timer -ge 5
+            echo -s "$resetColor" "$nMagenta" "◴ duration: " "$nGreen" "$timer" "s " "$resetColor"
+        end
+    end
 
     # new line
 	echo -s "$nWhite"
 
     # user
+    set -l userStyle "$nBlue"
+    if test (id -u "$USER")
+        set -l userStyle "$bRed"
+    end
     if test ! -z "$SSH_CLIENT"
-        echo -sn "$nBlue$USER$nCyan@$nWhite" (hostname)
+        echo -sn "$userStyle$USER$nCyan@$nWhite" (hostname)
     else
-        echo -sn "$nBlue$USER"
+        echo -sn "$userStyle$USER"
     end
 
     # separator
@@ -41,9 +65,9 @@ function fish_prompt
     echo -s "$resetColor"
     if test "$lastStatus" -eq 0
         if test (id -u "$USER") -eq 0
-            echo -sn "$bRed»"
+            echo -sn "$userStyle»"
         else
-            echo -sn "$nBlue›"
+            echo -sn "$userStyle›"
         end
     else
         echo -sn "$bRed⌦"
