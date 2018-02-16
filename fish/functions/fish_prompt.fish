41b4490b91512d9bcc3c4fdfb92c77a730ceab76
leny <info@flatland.be>
2018-02-16 20:16:29 +0100

    Add path in fish_prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,12 +1,15 @@
 function fish_prompt
-    set -g fish_prompt_pwd_dir_length 4
+    set -g fish_prompt_pwd_dir_length 2
     set -l lastStatus $status
 
+    set -l resetColor (set_color normal)
     set -l nWhite (set_color white)
     set -l nBlue (set_color blue)
     set -l bRed (set_color -o red)
     set -l nCyan (set_color cyan)
+    set -l bCyan (set_color --bold cyan)
     set -l nYellow (set_color yellow)
+    set -l nMagenta (set_color magenta)
 
     # new line
 	echo -s "$nWhite"
@@ -22,14 +25,20 @@ function fish_prompt
     echo -sn "$nYellow • $nWhite"
 
     # current directory
-    if test (pwd) = "$HOME/.pwendok"
+    if set -l match (string match -r "^$HOME\/Works\/([^\/]+)\/([^\/]+)\/(.+)\$" (pwd))
+        echo -sn "$nMagenta" "$match[2]" "$nWhite" "/" "$bCyan" "$match[3]" "$resetColor" "$nYellow" ":" "$nCyan" "$match[4]"
+    else if set -l match (string match -r "^$HOME\/Works\/([^\/]+)\/([^\/]+)\$" (pwd))
+        echo -sn "$nMagenta" "$match[2]" "$nWhite" "/" "$bCyan" "$match[3]" "$resetColor"
+    else if set -l match (string match -r "^$HOME\/Works\/([^\/]+)\$" (pwd))
+        echo -sn "$nMagenta" "$match[2]" "$resetColor"
+    else if test (pwd) = "$HOME/.pwendok"
         echo -sn "$bRed" "pwendok" "$nWhite"
     else
         echo -sn "$nCyan" (prompt_pwd) "$nWhite"
     end
 
     # invite
-    echo -s ""
+    echo -s "$resetColor"
     if test "$lastStatus" -eq 0
         if test (id -u "$USER") -eq 0
             echo -sn "$bRed»"
@@ -40,5 +49,5 @@ function fish_prompt
         echo -sn "$bRed⌦"
     end
 
-    echo -s "$nWhite "
+    echo -s "$resetColor "
 end
