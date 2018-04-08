82cb36db101907b24d320154c15ba99063e00505
leny <info@flatland.be>
2018-04-08 16:54:31 +0200

    Add GOPATH & GOBIN custom globals vars

--- fish/config.fish
+++ fish/config.fish
@@ -8,3 +8,6 @@ set -x WORKS_PATH "$HOME/Works"
 set -x GREP_OPTIONS "--color=auto"
 set -x HOMEBREW_CASK_OPTS "--appdir=/Applications"
 set -x PATH (yarn global bin) (brew --prefix coreutils)/libexec/gnubin /usr/local/sbin $PATH
+
+set -x -U GOPATH $HOME/.go
+set -x -U GOBIN $GOPATH/bin
