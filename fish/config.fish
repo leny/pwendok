9f301e04275dfc0702d544255c499aa7963c303f
leny <info@flatland.be>
2018-04-13 23:12:35 +0200

    Update GOPATH

--- fish/config.fish
+++ fish/config.fish
@@ -4,10 +4,10 @@ set -x EDITOR "vim"
 set -x CLICOLOR 1
 set -x LSCOLORS gxfxcxdxbxegedabagacad
 
-set -x WORKS_PATH "$HOME/Works"
+set -x WORKS_PATH $HOME/Works
 set -x GREP_OPTIONS "--color=auto"
 set -x HOMEBREW_CASK_OPTS "--appdir=/Applications"
 set -x PATH (yarn global bin) (brew --prefix coreutils)/libexec/gnubin /usr/local/sbin $PATH
 
-set -x -U GOPATH $HOME/.go
+set -x -U GOPATH $WORKS_PATH/go
 set -x -U GOBIN $GOPATH/bin
