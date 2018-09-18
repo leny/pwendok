b517bd0aca3fedd3eb4aa620f13a254986b2cade
leny <info@flatland.be>
2018-09-18 17:33:10 +0200

    Add tmux resurrect

--- install.sh
+++ install.sh
@@ -83,6 +83,8 @@ nvim +PlugInstall +qall
 
 echo ""
 echo "----- configure & install tmux -----"
+mkdir -p ~/.config/tmux-plugins
+git clone https://github.com/tmux-plugins/tmux-resurrect ~/.config/tmux-plugins/tmux-resurrect
 git clone https://github.com/leny/dotmux.git ~/.dotmux
 ln -sfv ~/.dotmux/tmux.conf ~/.tmux.conf
 cd ~/.dotmux
