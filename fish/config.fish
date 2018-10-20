ff87c6e6a4e0e1bd4fbc316e3ec95866c9af2944
leny <info@flatland.be>
2018-10-20 10:32:34 +0200

    Fix GOROOT in fish config

--- fish/config.fish
+++ fish/config.fish
@@ -9,6 +9,7 @@ set -x GREP_OPTIONS "--color=auto"
 set -x HOMEBREW_CASK_OPTS "--appdir=/Applications"
 set -x PATH (yarn global bin) (brew --prefix coreutils)/libexec/gnubin /usr/local/sbin $PATH
 
+set -x -U GOROOT /usr/local/Cellar/go/1.11/libexec
 set -x -U GOPATH $WORKS_PATH/go
 set -x -U GOBIN $GOPATH/bin
 
