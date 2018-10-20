df0d0ab9ebb1905594c1bdf4e2b2e7f757dff4e4
leny <info@flatland.be>
2018-10-20 10:33:59 +0200

    Fix LANG value for git/hub cli

--- fish/functions/git.fish
+++ fish/functions/git.fish
@@ -1,5 +1,6 @@
 # Wrap git automatically by adding the following to ~/.config/fish/functions/git.fish:
 
 function git --wraps hub --description 'Alias for hub, which wraps git to provide extra functionality with GitHub.'
+    export LANG=en_US.utf8
     hub $argv
 end
