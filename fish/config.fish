46ae68efa187342bd44ae340899ab3703166ace9
leny <info@flatland.be>
2019-03-28 00:02:32 +0100

    Add python in PATH

--- fish/config.fish
+++ fish/config.fish
@@ -7,7 +7,7 @@ set -x LSCOLORS gxfxcxdxbxegedabagacad
 set -x WORKS_PATH $HOME/Works
 set -x GREP_OPTIONS "--color=auto"
 set -x HOMEBREW_CASK_OPTS "--appdir=/Applications"
-set -x PATH (yarn global bin) (brew --prefix coreutils)/libexec/gnubin /usr/local/sbin $PATH
+set -x PATH (yarn global bin) (brew --prefix coreutils)/libexec/gnubin /Users/leny/Library/Python/3.7/bin /usr/local/sbin $PATH
 
 set -x -U GOROOT /usr/local/Cellar/go/1.11/libexec
 set -x -U GOPATH $WORKS_PATH/go
