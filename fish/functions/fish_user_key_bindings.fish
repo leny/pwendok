0201da7680eae3b44068310f5cc8c47dbc6d52e0
leny <info@flatland.be>
2019-04-25 09:01:06 +0200

    Add shortcut for editing command in editor

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -2,6 +2,10 @@ function fish_user_key_bindings
     # cf. http://lkdjiin.github.io/blog/2017/02/28/fish-completion-puis-execution-en-
 	# Ctrl+g (Go!). Like Ctrl+f Enter in one go.
     bind \cg accept-autosuggestion execute
+
+    # cf. https://github.com/fish-shell/fish-shell/issues/5030
+    # Ctrl+e - edit current command in editor
+    bind \ce edit_command_buffer
 end
 
 fzf_key_bindings
