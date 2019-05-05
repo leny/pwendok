e957a8e958aa093ce53df9b94fc9ce3e4d403c7a
Pierre-Antoine Delnatte <info@flatland.be>
2019-05-05 18:26:57 +0200

    Remove deprecated GREP_OPTIONS

--- fish/config.fish
+++ fish/config.fish
@@ -5,7 +5,6 @@ set -x CLICOLOR 1
 set -x LSCOLORS gxfxcxdxbxegedabagacad
 
 set -x WORKS_PATH $HOME/Works
-set -x GREP_OPTIONS "--color=auto"
 set -x HOMEBREW_CASK_OPTS "--appdir=/Applications"
 set -x PATH (yarn global bin) (brew --prefix coreutils)/libexec/gnubin /Users/leny/Library/Python/3.7/bin /usr/local/sbin $PATH
 
