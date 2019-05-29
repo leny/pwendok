00a5c22da766ab3352d869d12a3cde636920dbc5
leny <info@flatland.be>
2019-05-29 14:43:58 +0200

    Add git log aliases

---
+++ fish/functions/glog.fish
@@ -0,0 +1,3 @@
+function glog --description 'git log --oneline'
+    git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit
+end
