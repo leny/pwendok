83291afc2492978a9ca7a6f13745c85644bbf024
leny <info@flatland.be>
2019-09-05 14:00:32 +0200

    Save linux-specific modifications

--- fish/config.fish
+++ fish/config.fish
@@ -5,8 +5,7 @@ set -x CLICOLOR 1
 set -x LSCOLORS gxfxcxdxbxegedabagacad
 
 set -x WORKS_PATH $HOME/Works
-set -x HOMEBREW_CASK_OPTS "--appdir=/Applications"
-set -x PATH (yarn global bin) (brew --prefix coreutils)/libexec/gnubin /Users/leny/Library/Python/3.7/bin /usr/local/sbin $PATH
+set -x PATH /home/leny/.npm-global/bin /usr/local/sbin $PATH
 
 set -x -U GOROOT /usr/local/Cellar/go/1.11/libexec
 set -x -U GOPATH $WORKS_PATH/go
@@ -14,15 +13,4 @@ set -x -U GOBIN $GOPATH/bin
 
 set -x -U FZF_DEFAULT_COMMAND 'ag --hidden --ignore .git -g ""'
 
-# tabtab source for serverless package
-# uninstall by removing these lines or running `tabtab uninstall serverless`
-[ -f /usr/local/lib/node_modules/serverless/node_modules/tabtab/.completions/serverless.fish ]; and . /usr/local/lib/node_modules/serverless/node_modules/tabtab/.completions/serverless.fish
-# tabtab source for sls package
-# uninstall by removing these lines or running `tabtab uninstall sls`
-[ -f /usr/local/lib/node_modules/serverless/node_modules/tabtab/.completions/sls.fish ]; and . /usr/local/lib/node_modules/serverless/node_modules/tabtab/.completions/sls.fish
-
 thefuck --alias | source
-
-# tabtab source for slss package
-# uninstall by removing these lines or running `tabtab uninstall slss`
-[ -f /Users/leny/Works/becode/becodeorg-stats/node_modules/tabtab/.completions/slss.fish ]; and . /Users/leny/Works/becode/becodeorg-stats/node_modules/tabtab/.completions/slss.fish
