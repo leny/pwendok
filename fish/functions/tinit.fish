36c9b2b77168f9cfd5f2e5ece85c7a32150f5296
leny <info@flatland.be>
2025-05-02 00:30:45 +0200

    Add dev note

--- fish/functions/tinit.fish
+++ fish/functions/tinit.fish
@@ -6,6 +6,7 @@ function tinit -d "init a tmux session for given project"
         return 1
     end
 
+    # Calling fish at the end to ensure the window will still stay open if we quit nvim
     tmux new-session -s $session -d -n editors "cd $WORKS_PATH/$session ; nvim . ; fish"
 
     set window 2
