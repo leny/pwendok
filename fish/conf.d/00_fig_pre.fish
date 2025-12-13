270462871476e77dabf67ed637a8724695488bfd
leny <info@flatland.be>
2025-12-13 09:28:07 +0100

    Fix transition from aws q to kiro

--- fish/conf.d/00_fig_pre.fish
+++ fish/conf.d/00_fig_pre.fish
@@ -1,1 +1,1 @@
-test -x ~/.local/bin/q; and eval (~/.local/bin/q init fish pre --rcfile 00_fig_pre | string split0)
+test -x ~/.local/bin/kiro-cli; and eval (~/.local/bin/kiro-cli init fish pre --rcfile 00_fig_pre | string split0)
